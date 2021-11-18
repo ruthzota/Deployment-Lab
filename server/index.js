c949d4ffb3355a7d7098f4de4c2fdc9fad22a4e6
ruthfee zota <ruthfeezota@gmail.com>
2021-11-18 12:53:49 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.static("client"));
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  });
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+
+const port = process.env.PORT || process.env.SERVER_PORT;
+
+app.listen(port, () => console.log(`Yo server running on port ${port}`)); 
+``
